9084dce3774f6cc0441a4f3e41911eecec27acc4
ESX1234 <ESX1234@DESKTOP-KUP00MC>
2024-07-27 01:41:27 +0200

    added Single Product description

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,16 +1,38 @@
 import {ProductImage} from "@/components/ProductImage";
+import {Separator} from "@/components/ui/separator";
 
 export default function SinglePage() {
     return (
-        <div className="container mx-auto relative flex flex-col lg:flex-row gap-16">
+        <div className="container mx-auto relative flex flex-col lg:flex-row gap-16 pt-12">
 
-        {/*    IMAGE*/}
-            <div className="w-1/3">
+            {/*    IMAGE*/}
+            <div className="w-3/4 lg:w-1/3 mx-auto">
                 <ProductImage/>
             </div>
-        {/*    TEXT*/}
+            {/*    TEXT*/}
+            <div className="flex flex-col">
+                <h2 className="">Akatsuki T-shirt</h2>
+                <p className="text-gray-500">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                    Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+                    eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+                    accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <Separator />
+                <div className="mt-12 flex gap-4 items-center">
+                    <h4 className="text-xl text-gray-500 line-through">
+                        45€
+                    </h4>
+                    <h3 className="font-bold ">
+                        40.50€
+                    </h3>
+                </div>
+            {/*    ProductCustomizer*/}
 
-        {/*    Reviews*/}
+            </div>
+            {/*    Reviews*/}
         </div>
     );
 }
